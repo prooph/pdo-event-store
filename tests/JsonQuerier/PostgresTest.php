2f1d6dbc3b6aa2a20b38d1fe2103c0b5fa1dc743
Jefersson Nathan <admin@phpse.net>
2016-11-10 21:27:31 -0300

    Added tests for Postgres json query select

---
+++ tests/JsonQuerier/PostgresTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the prooph/pdo-event-store.
+ * (c) 2016-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\PDO;
+
+use Prooph\EventStore\PDO\JsonQuerier\Postgres;
+use PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * @covers \Prooph\EventStore\PDO\JsonQuerier\Postgres
+ */
+final class PostgresTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_select_data_from_metadata()
+    {
+        $sut = new Postgres();
+        self::assertSame('metadata->>\'metadata_data\'', $sut->metadata('metadata_data'));
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_select_data_from_payload()
+    {
+        $sut = new Postgres();
+        self::assertSame('payload->>\'payload_data\'', $sut->payload('payload_data'));
+    }
+}
