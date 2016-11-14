61578bebfb8dee4032da6abac22bc35e6aa589d5
prolic <saschaprolic@googlemail.com>
2016-11-14 17:07:20 +0800

    add missing exception

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the prooph/pdo-event-store.
+ * (c) 2016-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\EventStore\PDO\Exception;
+
+use Prooph\EventStore\Exception\InvalidArgumentException as EventStoreInvalidArgumentException;
+
+class InvalidArgumentException extends EventStoreInvalidArgumentException implements PDOEventStoreException
+{
+}
