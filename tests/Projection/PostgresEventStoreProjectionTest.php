5fbe8f575ef9493efb75a9b8e240ea42d707a77d
prolic <saschaprolic@googlemail.com>
2017-01-10 15:38:16 +0800

    fix test namespaces

--- tests/Projection/PostgresEventStoreProjectionTest.php
+++ tests/Projection/PostgresEventStoreProjectionTest.php
@@ -10,7 +10,7 @@
 
 declare(strict_types=1);
 
-namespace ProophTest\EventStore\Projection;
+namespace ProophTest\EventStore\Pdo\Projection;
 
 use Prooph\Common\Messaging\FQCNMessageFactory;
 use Prooph\Common\Messaging\NoOpMessageConverter;
