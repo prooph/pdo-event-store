534e2cdbb8be7ac4ed6992d7ef1b5d2b5ebae0ed
prolic <saschaprolic@googlemail.com>
2017-05-31 18:47:03 +0800

    fix mariadb schema

--- scripts/mariadb/02_projections_table.sql
+++ scripts/mariadb/02_projections_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `projections` (
   `no` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(150) NOT NULL,
-  `position` JSON,
-  `state` JSON,
+  `position` TEXT,
+  `state` TEXT,
   `status` VARCHAR(28) NOT NULL,
   `locked_until` CHAR(26),
-  CHECK (`position` IS NOT NULL OR JSON_VALID(`position`)),
-  CHECK (`state` IS NOT NULL OR JSON_VALID(`state`)),
+  CHECK (`position` IS NULL OR JSON_VALID(`position`)),
+  CHECK (`state` IS NULL OR JSON_VALID(`state`)),
   PRIMARY KEY (`no`),
   UNIQUE KEY `ix_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
