7779d0eeb5869aec9abd72a72ba6d40bece0eb95
prolic <saschaprolic@googlemail.com>
2017-09-19 15:06:50 +0800

    add libmariadbd-dev

--- .travis.install-mariadb-10.2.sh
+++ .travis.install-mariadb-10.2.sh
@@ -8,4 +8,4 @@ sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://download.nus.edu.sg/mirror/mariadb/repo/10.2/ubuntu trusty main'
 sudo apt-get update
-sudo apt-get install mariadb-server
+sudo apt-get install mariadb-server libmariadbd-dev
