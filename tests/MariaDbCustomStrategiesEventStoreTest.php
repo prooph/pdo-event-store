a9173b0d74b55c9a51ceade5c358c504bc1d8d2f
Bas Kamer <bas@bushbaby.nl>
2018-02-05 17:11:49 +0100

    added tests which fail

--- tests/MariaDbCustomStrategiesEventStoreTest.php
+++ tests/MariaDbCustomStrategiesEventStoreTest.php
@@ -13,25 +13,29 @@ declare(strict_types=1);
 namespace ProophTest\EventStore\Pdo;
 
 use ArrayIterator;
+use PDO;
+use Prooph\Common\Messaging\FQCNMessageFactory;
 use Prooph\EventStore\Exception\ConcurrencyException;
 use Prooph\EventStore\Metadata\MetadataMatcher;
 use Prooph\EventStore\Metadata\Operator;
-use Prooph\EventStore\Pdo\MySqlEventStore;
+use Prooph\EventStore\Pdo\Exception\RuntimeException;
+use Prooph\EventStore\Pdo\MariaDbEventStore;
+use Prooph\EventStore\Pdo\PersistenceStrategy;
 use Prooph\EventStore\Stream;
 use Prooph\EventStore\StreamName;
 use ProophTest\EventStore\Mock\UserCreated;
 use ProophTest\EventStore\Mock\UsernameChanged;
-use ProophTest\EventStore\Pdo\Assets\PersistenceStrategy\CustomMySqlAggregateStreamStrategy;
-use ProophTest\EventStore\Pdo\Assets\PersistenceStrategy\CustomMySqlSingleStreamStrategy;
+use ProophTest\EventStore\Pdo\Assets\PersistenceStrategy\CustomMariaDbAggregateStreamStrategy;
+use ProophTest\EventStore\Pdo\Assets\PersistenceStrategy\CustomMariaDbSingleStreamStrategy;
 use Ramsey\Uuid\Uuid;
 
 /**
- * @group mysql
+ * @group mariadb
  */
-final class MySqlCustomStrategiesEventStoreTest extends MySqlEventStoreTest
+final class MariaDbCustomStrategiesEventStoreTest extends MariaDbEventStoreTest
 {
     /**
-     * @var MySqlEventStore
+     * @var MariaDbEventStore
      */
     protected $eventStore;
 
@@ -42,9 +46,28 @@ final class MySqlCustomStrategiesEventStoreTest extends MySqlEventStoreTest
         }
 
         $this->connection = TestUtil::getConnection();
-        TestUtil::initCustomDatabaseTables($this->connection);
+        TestUtil::initDefaultDatabaseTables($this->connection);
 
-        $this->setupEventStoreWith(new CustomMySqlAggregateStreamStrategy());
+        $this->setupEventStoreWith(new CustomMariaDbAggregateStreamStrategy());
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_create_new_stream_if_table_name_is_already_used(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Error during createSchemaFor');
+
+        $streamName = new StreamName('foo');
+        $schema = $this->persistenceStrategy->createSchema($this->persistenceStrategy->generateTableName($streamName));
+
+        foreach ($schema as $command) {
+            $statement = $this->connection->prepare($command);
+            $statement->execute();
+        }
+
+        $this->eventStore->create(new Stream($streamName, new \ArrayIterator()));
     }
 
     /**
@@ -52,7 +75,7 @@ final class MySqlCustomStrategiesEventStoreTest extends MySqlEventStoreTest
      */
     public function it_loads_correctly_using_single_stream_per_aggregate_type_strategy(): void
     {
-        $this->setupEventStoreWith(new CustomMySqlSingleStreamStrategy(), 5);
+        $this->setupEventStoreWith(new CustomMariaDbSingleStreamStrategy(), 5);
 
         $streamName = new StreamName('Prooph\Model\User');
 
@@ -83,7 +106,7 @@ final class MySqlCustomStrategiesEventStoreTest extends MySqlEventStoreTest
     {
         $this->expectException(ConcurrencyException::class);
 
-        $this->setupEventStoreWith(new CustomMySqlSingleStreamStrategy());
+        $this->setupEventStoreWith(new CustomMariaDbSingleStreamStrategy());
 
         $streamEvent = UserCreated::with(
             ['name' => 'Max Mustermann', 'email' => 'contact@prooph.de'],
@@ -110,5 +133,52 @@ final class MySqlCustomStrategiesEventStoreTest extends MySqlEventStoreTest
         $streamEvent = $streamEvent->withAddedMetadata('_aggregate_type', 'user');
 
         $this->eventStore->appendTo(new StreamName('Prooph\Model\User'), new ArrayIterator([$streamEvent]));
+    }
+
+    public function it_ignores_transaction_handling_if_flag_is_enabled(): void
+    {
+        $connection = $this->prophesize(PDO::class);
+        $connection->beginTransaction()->shouldNotBeCalled();
+        $connection->commit()->shouldNotBeCalled();
+        $connection->rollback()->shouldNotBeCalled();
+
+        $eventStore = new MariaDbEventStore(new FQCNMessageFactory(), $connection->reveal(), new CustomMariaDbAggregateStreamStrategy());
+
+        $streamEvent = UserCreated::with(
+            ['name' => 'Max Mustermann', 'email' => 'contact@prooph.de'],
+            1
+        );
+
+        $stream = new Stream(new StreamName('Prooph\Model\User'), new ArrayIterator([$streamEvent]));
+
+        $eventStore->create($stream);
+
+        $streamEvent = UsernameChanged::with(
+            ['name' => 'John Doe'],
+            1
+        );
+
+        $eventStore->appendTo(new StreamName('Prooph\Model\User'), new ArrayIterator([$streamEvent]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_removes_stream_if_stream_table_hasnt_been_created(): void
+    {
+        $strategy = $this->createMock(PersistenceStrategy::class);
+        $strategy->method('createSchema')->willReturn(["SIGNAL SQLSTATE '45000';"]);
+        $strategy->method('generateTableName')->willReturn('_non_existing_table');
+
+        $this->setupEventStoreWith($strategy);
+
+        $stream = new Stream(new StreamName('Prooph\Model\User'), new ArrayIterator());
+
+        try {
+            $this->eventStore->create($stream);
+        } catch (RuntimeException $e) {
+        }
+
+        $this->assertFalse($this->eventStore->hasStream($stream->streamName()));
     }
 }
--- tests/MariaDbEventStoreTest.php
+++ tests/MariaDbEventStoreTest.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * This file is part of the prooph/pdo-event-store.
- * (c) 2016-2017 prooph software GmbH <contact@prooph.de>
- * (c) 2016-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2016-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -32,7 +32,7 @@ use Ramsey\Uuid\Uuid;
 /**
  * @group mariadb
  */
-final class MariaDbEventStoreTest extends AbstractPdoEventStoreTest
+class MariaDbEventStoreTest extends AbstractPdoEventStoreTest
 {
     /**
      * @var MariaDbEventStore
