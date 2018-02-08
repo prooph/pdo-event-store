1c852f4fe45b80055f2f26ffe53344e3ac238b4f
Bas Kamer <bas@bushbaby.nl>
2018-02-08 14:40:15 +0100

    mariadb with custom tables

---
+++ tests/Projection/MariaDbEventStoreProjectorCustomTablesTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of the prooph/pdo-event-store.
+ * (c) 2016-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\Pdo\Projection;
+
+use Prooph\Common\Messaging\FQCNMessageFactory;
+use Prooph\EventStore\Pdo\MariaDbEventStore;
+use Prooph\EventStore\Pdo\PersistenceStrategy\MariaDbSimpleStreamStrategy;
+use Prooph\EventStore\Pdo\Projection\MariaDbProjectionManager;
+use ProophTest\EventStore\Mock\UserCreated;
+use ProophTest\EventStore\Pdo\TestUtil;
+
+/**
+ * @group mariadb
+ */
+class MariaDbEventStoreProjectorCustomTablesTest extends PdoEventStoreProjectorCustomTablesTest
+{
+    protected function setUp(): void
+    {
+        if (TestUtil::getDatabaseDriver() !== 'pdo_mysql') {
+            throw new \RuntimeException('Invalid database driver');
+        }
+
+        $this->connection = TestUtil::getConnection();
+        TestUtil::initCustomDatabaseTables($this->connection);
+
+        $this->eventStore = new MariaDbEventStore(
+            new FQCNMessageFactory(),
+            $this->connection,
+            new MariaDbSimpleStreamStrategy(),
+            10000,
+            'estreams'
+        );
+
+        $this->projectionManager = new MariaDbProjectionManager(
+            $this->eventStore,
+            $this->connection,
+            'estreams',
+            'eprojections'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_missing_projection_table(): void
+    {
+        $this->expectException(\Prooph\EventStore\Pdo\Exception\RuntimeException::class);
+        $this->expectExceptionMessage("Error 42S02. Maybe the projection table is not setup?\nError-Info: Table 'event_store_tests.eprojections' doesn't exist");
+
+        $this->prepareEventStream('user-123');
+
+        $this->connection->exec('DROP TABLE eprojections;');
+
+        $projection = $this->projectionManager->createProjection('test_projection');
+
+        $projection
+            ->fromStream('user-123')
+            ->when([
+                UserCreated::class => function (array $state, UserCreated $event): array {
+                    $this->stop();
+
+                    return $state;
+                },
+            ])
+            ->run();
+    }
+}
---
+++ tests/Projection/PdoEventStoreProjectorCustomTablesTest.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * This file is part of the prooph/pdo-event-store.
+ * (c) 2016-2018 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\EventStore\Pdo\Projection;
+
+use PDO;
+use Prooph\Common\Messaging\Message;
+use Prooph\EventStore\EventStore;
+use Prooph\EventStore\EventStoreDecorator;
+use Prooph\EventStore\Exception\InvalidArgumentException;
+use Prooph\EventStore\Pdo\Projection\PdoEventStoreProjector;
+use Prooph\EventStore\Projection\ProjectionManager;
+use Prooph\EventStore\Projection\Projector;
+use ProophTest\EventStore\Mock\UserCreated;
+use ProophTest\EventStore\Mock\UsernameChanged;
+use ProophTest\EventStore\Pdo\TestUtil;
+use ProophTest\EventStore\Projection\AbstractEventStoreProjectorTest;
+
+abstract class PdoEventStoreProjectorCustomTablesTest extends AbstractEventStoreProjectorTest
+{
+    /**
+     * @var ProjectionManager
+     */
+    protected $projectionManager;
+
+    /**
+     * @var EventStore
+     */
+    protected $eventStore;
+
+    /**
+     * @var PDO
+     */
+    protected $connection;
+
+    protected function tearDown(): void
+    {
+        TestUtil::tearDownDatabase();
+    }
+
+    /**
+     * @test
+     */
+    public function it_updates_state_using_when_and_persists_with_block_size(): void
+    {
+        $this->prepareEventStream('user-123');
+
+        $testCase = $this;
+
+        $projection = $this->projectionManager->createProjection('test_projection', [
+            Projector::OPTION_PERSIST_BLOCK_SIZE => 10,
+        ]);
+
+        $projection
+            ->fromAll()
+            ->when([
+                UserCreated::class => function ($state, Message $event) use ($testCase): array {
+                    $testCase->assertEquals('user-123', $this->streamName());
+                    $state['name'] = $event->payload()['name'];
+
+                    return $state;
+                },
+                UsernameChanged::class => function ($state, Message $event) use ($testCase): array {
+                    $testCase->assertEquals('user-123', $this->streamName());
+                    $state['name'] = $event->payload()['name'];
+
+                    if ($event->payload()['name'] === 'Sascha') {
+                        $this->stop();
+                    }
+
+                    return $state;
+                },
+            ])
+            ->run();
+
+        $this->assertEquals('Sascha', $projection->getState()['name']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_handles_existing_projection_table(): void
+    {
+        $this->prepareEventStream('user-123');
+
+        $projection = $this->projectionManager->createProjection('test_projection');
+
+        $projection
+            ->init(function (): array {
+                return ['count' => 0];
+            })
+            ->fromAll()
+            ->when([
+                UsernameChanged::class => function (array $state, Message $event): array {
+                    $state['count']++;
+
+                    return $state;
+                },
+            ])
+            ->run(false);
+
+        $this->assertEquals(49, $projection->getState()['count']);
+
+        $projection->run(false);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_exception_when_invalid_wrapped_event_store_instance_passed(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unknown event store instance given');
+
+        $eventStore = $this->prophesize(EventStore::class);
+        $wrappedEventStore = $this->prophesize(EventStoreDecorator::class);
+        $wrappedEventStore->getInnerEventStore()->willReturn($eventStore->reveal())->shouldBeCalled();
+
+        new PdoEventStoreProjector(
+            $wrappedEventStore->reveal(),
+            $this->connection,
+            'test_projection',
+            'event_streams',
+            'projections',
+            1,
+            1,
+            1,
+            1
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_exception_when_unknown_event_store_instance_passed(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Unknown event store instance given');
+
+        $eventStore = $this->prophesize(EventStore::class);
+        $connection = $this->prophesize(PDO::class);
+
+        new PdoEventStoreProjector(
+            $eventStore->reveal(),
+            $connection->reveal(),
+            'test_projection',
+            'event_streams',
+            'projections',
+            10,
+            10,
+            10,
+            10000
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_dispatches_pcntl_signals_when_enabled(): void
+    {
+        if (! extension_loaded('pcntl')) {
+            $this->markTestSkipped('The PCNTL extension is not available.');
+
+            return;
+        }
+
+        $command = 'exec php ' . realpath(__DIR__) . '/isolated-projection.php';
+        $descriptorSpec = [
+            0 => ['pipe', 'r'],
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ];
+        /**
+         * Created process inherits env variables from this process.
+         * Script returns with non-standard code SIGUSR1 from the handler and -1 else
+         */
+        $projectionProcess = proc_open($command, $descriptorSpec, $pipes);
+        $processDetails = proc_get_status($projectionProcess);
+        sleep(1);
+        posix_kill($processDetails['pid'], SIGQUIT);
+        sleep(1);
+
+        $processDetails = proc_get_status($projectionProcess);
+        $this->assertEquals(
+            SIGUSR1,
+            $processDetails['exitcode']
+        );
+    }
+}
