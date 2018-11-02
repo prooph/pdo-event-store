346abafb8f63a0560caf35225d8a47986f586049
Bas Kamer <bas@bushbaby.nl>
2018-11-02 14:30:35 +0100

    apply prooph cs-fixer

--- tests/Assets/PersistenceStrategy/CustomMariaDbSingleStreamStrategy.php
+++ tests/Assets/PersistenceStrategy/CustomMariaDbSingleStreamStrategy.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the prooph/pdo-event-store.
+ * This file is part of prooph/pdo-event-store.
  * (c) 2016-2018 prooph software GmbH <contact@prooph.de>
  * (c) 2016-2018 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
